14dd3910ae75c6a5f51f977f4f849031328eaa44
findm3wh0 <findm3wh0@gmail.com>
2025-12-14 15:20:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+def print_numbers(n):
+    """
+    Students: Implement this function!
+    Print integers from 1 to n with no spaces.
+    Use only print(i, end='') in a loop — no strings allowed! 🥺
+    """
+    # Your code here!
+    pass  # Delete this when you start coding~
+
+
+if __name__ == '__main__':
+    # This part runs only for manual testing or real submission
